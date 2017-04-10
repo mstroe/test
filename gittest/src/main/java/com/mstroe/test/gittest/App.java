50386fa011ead19e8a6237284c1e323614ba117f
mstroe <mariusstroe@macbook-pro.asus.myname>
2017-04-10 19:53:44 -0400

    commit message

---
+++ gittest/src/main/java/com/mstroe/test/gittest/App.java
@@ -0,0 +1,13 @@
+package com.mstroe.test.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
