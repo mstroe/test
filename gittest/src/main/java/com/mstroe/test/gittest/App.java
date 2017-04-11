adacccdd24ebb0b81cee1fbf77af32bc378123d1
mstroe <mariusstroe@gmail.com>
2017-04-11 00:13:09 -0400

    to 2 in master

--- gittest/src/main/java/com/mstroe/test/gittest/App.java
+++ gittest/src/main/java/com/mstroe/test/gittest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World7! Feature 1 branch" );
+        System.out.println( "Hello World2!" );
     }
 }
