8b67eaa1db8362188a8b735f676efdafdac489db
mstroe <mariusstroe@macbook-pro.asus.myname>
2017-04-10 23:22:46 -0400

    feature1 commit

--- gittest/src/main/java/com/mstroe/test/gittest/App.java
+++ gittest/src/main/java/com/mstroe/test/gittest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World7!" );
+        System.out.println( "Hello World7! Feature 1 branch" );
     }
 }
